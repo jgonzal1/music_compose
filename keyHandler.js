4201a44a2d340032bb46e3dd9d3b5adf0e166261
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2021-03-18 00:49:27 +0100

    + instrumentCorelator, # dnbSounds as JSON, HTML room filling, ~linting

--- keyHandler.js
+++ keyHandler.js
@@ -1,3 +1,23 @@
+const instrumentCorrelator = {
+    ",":"🎼 Arp",
+    ".":"🥁 Drum",
+    "<":"🎤 Vocal",
+    "b":"🎹 Keyboard",
+    "B":"🎹 Keyboard",
+    "c":"🎷 Vibes",
+    "C":"🎷 Vibes",
+    "m":"🎻 String",
+    "M":"🎻 String",
+    "n":"🎺 Wind",
+    "N":"🎺 Wind",
+    "v":"🎸 Bass",
+    "V":"🎸 Bass",
+    "x":"🎵 Melody",
+    "X":"🎵 Melody",
+    "z":"🎧 Acidline",
+    "Z":"🎧 Acidline"
+}
+
 let buttonsPushed = 0;
 let keyChanged = false;
 let pushedKey;
@@ -23,9 +43,46 @@ $(document).on('keypress',
                 document.getElementById("likedSoundsPlaceholder").innerText +=
                     document.getElementById("nameOfSoundPlaceholder").innerText + "\n";
                 break;
+            case "<":
+                playAudio("Vocal");
+                break;
+            case "z":
+            case "Z":
+                playAudio("Acidline");
+                break;
+            case "x":
+            case "X":
+                playAudio("Melody");
+                break;
+            case "c":
+            case "C":
+                playAudio("Vibes");
+                break;
+            case "v":
+            case "V":
+                playAudio("Bass");
+                break;
+            case "b":
+            case "B":
+                playAudio("Keyboard");
+                break;
+            case "n":
+            case "N":
+                playAudio("Wind");
+                break;
+            case "m":
+            case "M":
+                playAudio("String");
+                break;
+            case ",":
+                playAudio("Arp");
+                break;
+            case ".":
+                playAudio("Drum");
+                break;
         }
         if (document.getElementById("pushedKey").innerText != pushedKey) {
-            document.getElementById("pushedKey").innerText = pushedKey;
+            document.getElementById("pushedKey").innerText = instrumentCorrelator[pushedKey];
             keyChanged = true;
         } else {
             keyChanged = false;
