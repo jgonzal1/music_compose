80d7d8b06c119a622d6f4dcecbf6e121f90727ce
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2021-09-11 06:39:06 +0200

    + spotify INSERT queries, PowerBI spotify querier, spotify api results data, bash_multiline_curl_example,  ←→ djaMusicGenerator, - ignoring PBIXs

---
+++ assets/bash_multiline_curl_example.bat
@@ -0,0 +1,8 @@
+# curl -X "GET" http://^
+# google.com -H "Accept: application/json"
+
+# echo curl -X "POST" https://api.spotify.com/v1/playlists/1yPKRK04fvqUpxBC7fPPOf/tracks?uris=^
+# spotify%3Atrack%3A4iV5W9uYEdYUVa79Axb7Rh%2C^
+# spotify%3Atrack%3A1301WleyT98MSxVHPZCA6M ^
+# -H "Accept: application/json" -H "Content-Type: application/json" ^
+# -H "Authorization: Bearer BQCPunhfp9Ops-gcGDlcOxiZPPO63v7G6e1NRzpJIivdd-FWbJQeb9fWavY0Ks1vGvGbYB4L8XNdNf9ogV61FQtytrc2zaPnKyL-ewWGRqH729YTXCKGvjM3_pjlHuYaz7a-iOjKK8GlP03jPNnkXg5Nhe6ys-I8UGm6oxxZDeHR5pL5uAzVWvYARoXLzTrhXOA1afEPSJMcfpbL68JhNFt_8A"
