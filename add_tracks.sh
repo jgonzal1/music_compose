a21e9567263c0d446466d12555998dc54a57c57f
Javier Gonzalez Berenguel <jgb@aety.io>
2022-04-19 14:00:14 +0200

    + add_tracks, # playlists lintings for descriptive fold view

---
+++ add_tracks.sh
@@ -0,0 +1,19 @@
+curl -X "POST" "https://api.spotify.com/v1/playlists/21SjFBAzyO7h9vrGyyMVC9/tracks" -H "Accept: application/json"\
+  -H "Content-Type: application/json"\
+  -H "Authorization: Bearer BQAKjAhMHIuPw88_Ci1TynLdyi-tXKTWQ22LHVUeE7od25C7kzF38fEkKPb3wFqwBQi7UbgjSymDJYj12NEWJIA67dGcLqDsHHhsAeC31VK84W9FGF174iHwKlkv53ZG-CnOTPIPSv21ltsiBho5bYtfkndY1bXhMqaEywh4YzyMNvTw9qY5Af2mB_TT_FYMQ2b-uU9TO40UWKzyijHj9qLqD8uvppiv7DaB8Me77dUuMHEp0LmeObw"\
+  --data '{
+  "uris": [
+    "spotify:track:1iifeqOXn72qsEIZkyfobl"
+    ,"spotify:track:1cp7pENCkTI63S6TIjFyVN"
+    ,"spotify:track:76mKu0rubd67KGFbaX9v6t"
+    ,"spotify:track:3fhEo4pmDF84y3wMEnp28G"
+    ,"spotify:track:2mCzxEUd3yhdOnY4rtSImT"
+    ,"spotify:track:0kX6bR50oNgWml677v75Ay"
+    ,"spotify:track:0cYHBMVxUmgB2z5YGlnERK"
+    ,"spotify:track:1uTUKDrXHNxAAcOmCOfUcN"
+    ,"spotify:track:5uEGBEtsHJJ2oyYj5nfs9O"
+    ,"spotify:track:3Qf8jb3ztF50Sk62Evhpkf"
+    ,"spotify:track:0Ahf9coejpMBiqgHYKPkg2"
+  ],
+  "position": 0
+}'
