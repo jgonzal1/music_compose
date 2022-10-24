7004e80189a1c9d8a8e0b6d182c803e345c5562b
Javier <javier.gonzalezberenguel@gmail.com>
2022-10-24 22:46:12 +0200

    Delete keys.js

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-const clientId = "37f7d88a10a045ca879d9ee501c1b7db";
-const clientSecret = "92d95ee6d9bc4ca2a7cfb4fda7d464f6";
-const playlistsBearer = /*"BQCLCu3AEC6dm3zdeLNca7x5BYpl2i9FMx8SgIybpIrSSU1k7HW"+
-  "-3T61Cw5LEOaY8MBxZqxHhIXN4ya3cQc";*/
-  "BQCIkkt9-tNEz6hRL7ukD4huqCtbcKQB3eiL6qbrAzsw2JUc5BIO-OLWzp_3k2MnrP9Ohq_9ulRdRe_YJbk";
