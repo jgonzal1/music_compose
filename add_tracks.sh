10f236ed4baefa8a3bb9fb4cd9e35cb66b763a3c
Javier <javier.gonzalezberenguel@gmail.com>
2022-10-25 11:43:14 +0200

    Removed bearer

--- add_tracks.sh
+++ add_tracks.sh
@@ -1,6 +1,6 @@
 curl -X "POST" "https://api.spotify.com/v1/playlists/21SjFBAzyO7h9vrGyyMVC9/tracks" -H "Accept: application/json"\
   -H "Content-Type: application/json"\
-  -H "Authorization: Bearer BQAKjAhMHIuPw88_Ci1TynLdyi-tXKTWQ22LHVUeE7od25C7kzF38fEkKPb3wFqwBQi7UbgjSymDJYj12NEWJIA67dGcLqDsHHhsAeC31VK84W9FGF174iHwKlkv53ZG-CnOTPIPSv21ltsiBho5bYtfkndY1bXhMqaEywh4YzyMNvTw9qY5Af2mB_TT_FYMQ2b-uU9TO40UWKzyijHj9qLqD8uvppiv7DaB8Me77dUuMHEp0LmeObw"\
+  -H "Authorization: Bearer {private}"\
   --data '{
   "uris": [
     "spotify:track:1iifeqOXn72qsEIZkyfobl"
